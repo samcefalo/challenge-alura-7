d6f7ec5c36f3f532a6fd886092dcc658c7d54288
SamCefalo <samuel.cefalo@gmail.com>
2023-07-24 21:14:08 -0300

    Initial commit

---
+++ src/main/java/me/samcefalo/challenge/alura/ChallengeAlura7Application.java
@@ -0,0 +1,13 @@
+package me.samcefalo.challenge.alura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeAlura7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeAlura7Application.class, args);
+    }
+
+}
